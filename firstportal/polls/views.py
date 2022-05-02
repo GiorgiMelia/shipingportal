8bdce8ad7796b600b3b1aac2bebd70d59d8a53be
nika <nikolozmelia@gmail.com>
2022-05-02 23:21:41 +0400

    test1

--- firstportal/polls/views.py
+++ firstportal/polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.1")
 
 def account(request,id=0):
     return HttpResponse("account returned "+str(id))
